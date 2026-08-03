eee8c438a8b7f75bebe96fbdb8c3e845dfc8dd1b
Wilmer <wil@sentinel.com>
2026-08-03 07:20:46 -0600

    Add Kumbaya (MegaETH) protocol review

--- src/components/author/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as AuthorCard } from "./author-card";
-export { default as AuthorProfile } from "./author-profile";
-export { default as AuthorLink } from "./author-link";
-export { PostItem } from "./post-item";
-export { getSocialIcon } from "./social-icons";
